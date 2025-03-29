02bca314a181c7ac77d1f8048db2ed63b62a73d0
Kenneth Crismon <kencrismon@crismonicwave.com>
2025-03-29 11:11:27 -0700

    - Ok, binding to all users in the dummy-users list dynamically, Upon click, the users-tasks is now presenting the name.

--- udemy/Angular-TheCompleteGuide2025Edition/01-starting-project/src/app/app.component.ts
+++ udemy/Angular-TheCompleteGuide2025Edition/01-starting-project/src/app/app.component.ts
@@ -3,15 +3,28 @@ import { NgFor } from '@angular/common';
 import { HeaderComponent } from "./header/header.component";
 import { UserComponent } from "./user/user.component";
 import { DUMMY_USERS } from './dummy-users';
+import { TasksComponent } from "./tasks/tasks.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, UserComponent, NgFor],
+  imports: [HeaderComponent, UserComponent, NgFor, TasksComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   users = DUMMY_USERS;
+  name = 'My Tasks';
+  selectedUserId = 'u1';
 
+  get selectedUser() {
+    const user = this.users.find(user => user.id === this.selectedUserId)!;
+    console.log("Selected user: " + user.name);
+    return user;
+  }
+
+  onUserSelected(id: string) {
+    console.log("User selected APP: " + id);
+    this.selectedUserId = id;
+  }
 }
--- udemy/Angular-TheCompleteGuide2025Edition/01-starting-project/src/app/user/user.component.ts
+++ udemy/Angular-TheCompleteGuide2025Edition/01-starting-project/src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, input, computed } from '@angular/core';
+import { Component, Input, Output, input, output, computed, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -8,16 +8,24 @@ import { Component, Input, input, computed } from '@angular/core';
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  avatar = input.required<string>()
-  name = input.required<string>()
+  // avatar = input.required<string>()
+  // name = input.required<string>()
+  // id = input.required<string>()
+  @Input({ required: true }) avatar!: string;
+  @Input({ required: true }) name!: string;
+  @Input({ required: true }) id!: string;
+
+  @Output() selectUser = new EventEmitter<string>(); // Changed from 'select' to 'selectUser'
+  //select = output<string>();
 
   imagePath = computed( () => {
-    return "assets/users/" + this.avatar();
+    return "assets/users/" + this.avatar;
   });
 
   onSelectedUser() {
-    console.log("User selected: " +
-      this.name());
+    console.log("User selected UC: " +
+      this.name + " " + this.id);
+    this.selectUser.emit(this.id);
   }
 
 }
